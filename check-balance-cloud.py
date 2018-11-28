394cb8ddf11780fa859e43386c5218ff1aa0f910
Marian Witkowski <marian.witkowski@gmail.com>
2018-11-28 19:01:53 +0100

    Update check-balance-cloud.py

--- check-balance-cloud.py
+++ check-balance-cloud.py
@@ -32,26 +32,35 @@ PASSWORD = "MySecretPassword"
 PHONE_NUMBER = "+48500600700"
 ACCESS_TOKEN = "Z3BaZqmfZef9FO********************"
 
+# open bank's webpage
 driver.get("https://sso.cloud.ideabank.pl/#/login")
+
+# find login input and type client ID
 elem = driver.find_element_by_name("login")
 elem.send_keys(CLIENT_ID)
 elem.send_keys(Keys.ENTER)
 time.sleep(2)
 
+# find password input and type value
 elem = driver.find_element_by_name("password")
 elem.send_keys(PASSWORD)
+
+# press ENTER - send form
 elem.send_keys(Keys.ENTER)
 
 try:
 
+    # wait for visibility of element with "account-current-balance" class
     element = WebDriverWait(driver, 300).until(
         EC.presence_of_element_located((By.CLASS_NAME, "account-current-balance"))
     )
 
+    # get value from "account-current-balance" element
     value = driver.execute_script(" return document.getElementsByClassName('account-current-balance')[0].innerText; ")
     currBalance = float(str(value).replace(" ","").replace("PLN","").replace(",",".") )
     prevBalance = prev_balance()
 
+    # send SMS only in current balance and previous balance is not equal
     if prevBalance is None or currBalance<>prev_balance:
         print currBalance
         save_balance(currBalance)
